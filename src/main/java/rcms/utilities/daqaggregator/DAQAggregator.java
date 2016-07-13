848cf63c76e024f8ae699e079e443aaaa4a8dc0e
gladky <maciej.gladki@gmail.com>
2016-07-13 13:59:58 +0200

    fix(): handling session id retrieval problem

--- src/main/java/rcms/utilities/daqaggregator/DAQAggregator.java
+++ src/main/java/rcms/utilities/daqaggregator/DAQAggregator.java
@@ -124,15 +124,17 @@ public class DAQAggregator {
 						logger.info("Done for session " + daq.getSessionId());
 					}
 
-					prepareAndPersistSnapshot(daq, flashlistManager, persistorManager);
-
+					if (flashlistManager != null) {
+						prepareAndPersistSnapshot(daq, flashlistManager, persistorManager);
+					} else {
+						logger.warn("Flashlist manager not initialized, session id not available");
+					}
 					// FIXME: the timer should be used here as sleep time !=
 					// period time
 					logger.info("sleeping for 2 seconds ....\n");
 					Thread.sleep(2000);
 				} catch (Exception e) {
-					logger.error("Error in main loop:\n" + e);
-					e.printStackTrace();
+					logger.error("Error in main loop:", e);
 					logger.info("Going to sleep for 10 seconds before trying again...\n");
 					Thread.sleep(10000);
 				}
