a44e352faca7ac33337b26f297f4cd9a538eb11c
gladky <maciej.gladki@gmail.com>
2016-08-05 15:38:03 +0200

    feat(persistence): persist in time-based directory structure

--- src/main/java/rcms/utilities/daqaggregator/persistence/PersistorManager.java
+++ src/main/java/rcms/utilities/daqaggregator/persistence/PersistorManager.java
@@ -1,17 +1,8 @@
 package rcms.utilities.daqaggregator.persistence;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
 
 import org.apache.log4j.Logger;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import rcms.utilities.daqaggregator.data.DAQ;
 
@@ -28,14 +19,16 @@ public class PersistorManager {
 	/** Persistence directory to work with */
 	protected final String persistenceDir;
 
-	private ObjectMapper objectMapper = new ObjectMapper();
-
-	private SnapshotFormat format;
+	private final SnapshotFormat format;
+	
+	protected final StructureSerializer persistor ;
 
 	/** Constructor */
 	public PersistorManager(String persistenceDir, SnapshotFormat format) {
+
 		this.persistenceDir = persistenceDir;
 		this.format = format;
+		this.persistor = new StructureSerializer();
 		instance = this;
 	}
 
@@ -47,24 +40,25 @@ public class PersistorManager {
 		return instance;
 	}
 
+	/**
+	 * Persist single DAQ snapshot. It will be persisted in time-based directory
+	 * structure based on timestamp from daq snapshot.
+	 * 
+	 * For instance snapshot with timestamp 1470234844087 (Wed Aug 03 16:34:04
+	 * CEST 2016) will be stored in /2016/8/3/16/ directory
+	 * 
+	 * 
+	 * @param daq
+	 *            snapshot to be persisted
+	 * @return absolute path to file where snapshot was persisted or null when
+	 *         persistence was not successfull
+	 */
 	public String persistSnapshot(DAQ daq) {
 
 		try {
-
-			String isoDate = objectMapper.writeValueAsString(new Date(daq.getLastUpdate()));
-
-			StructureSerializer persistor = new StructureSerializer();
 			String filename = null;
-			switch (format) {
-			case SMILE:
-				filename = persistor.serializeToSmile(daq, isoDate, persistenceDir);
-				break;
-			case JSON:
-				filename = persistor.serializeToJSON(daq, isoDate, persistenceDir);
-				break;
-			default:
-				logger.warn("Format of snapshot not available");
-			}
+			filename = persistor.serialize(daq, persistenceDir, format);
+
 			logger.info("Successfully persisted in " + persistenceDir + " as file " + filename);
 			return filename;
 		} catch (IOException e) {
@@ -72,66 +66,5 @@ public class PersistorManager {
 			return null;
 		}
 	}
-
-	/**
-	 * Converts files from one format to another
-	 */
-	public void convertSnapshots(String targetDirectory) throws IOException {
-
-		List<File> fileList = getFiles(persistenceDir);
-		Collections.sort(fileList, FileComparator);
-
-		StructureSerializer structurePersistor = new StructureSerializer();
-		DAQ daq;
-		logger.info("Converting files...");
-		for (File path : fileList) {
-			logger.debug(path.getName().toString());
-
-			daq = structurePersistor.deserializeFromSmile(path.getAbsolutePath().toString());
-			if (daq != null)
-				structurePersistor.serializeToSmile(daq, daq.getLastUpdate() + ".smile", targetDirectory);
-		}
-	}
-
-	/**
-	 * Get available file names from persistence folder
-	 * 
-	 * @return available file names from persistence folder
-	 * @throws IOException
-	 */
-	protected List<File> getFiles(String file) throws IOException {
-		List<File> result = new ArrayList<>();
-
-		File folder = new File(file);
-
-		if (folder.exists() && folder.isDirectory()) {
-
-			File[] listOfFiles = folder.listFiles();
-
-			for (int i = 0; i < listOfFiles.length; i++) {
-				if (listOfFiles[i].isFile()) {
-
-					result.add(listOfFiles[i]);
-				} else if (listOfFiles[i].isDirectory()) {
-					System.out.println("Directory " + listOfFiles[i].getName());
-				}
-			}
-
-			return result;
-		} else {
-			throw new FileNotFoundException("Folder does not exist " + folder.getAbsolutePath());
-		}
-	}
-
-	/**
-	 * Filename comparator
-	 */
-	public static Comparator<File> FileComparator = new Comparator<File>() {
-		public int compare(File path1, File path2) {
-			String filename1 = path1.getName().toString().toUpperCase();
-			String filename2 = path2.getName().toString().toUpperCase();
-			return filename1.compareTo(filename2);
-		}
-	};
 
 }
--- src/main/java/rcms/utilities/daqaggregator/persistence/StructureSerializer.java
+++ src/main/java/rcms/utilities/daqaggregator/persistence/StructureSerializer.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.apache.log4j.Logger;
 
@@ -52,81 +54,100 @@ public class StructureSerializer {
 
 	private static final Logger logger = Logger.getLogger(StructureSerializer.class);
 
-	
-	private void addRefMixins(ObjectMapper objectMapper){
-		objectMapper.addMixIn(BU.class, rcms.utilities.daqaggregator.data.mixin.ref.BUMixIn.class);
-		objectMapper.addMixIn(BUSummary.class, rcms.utilities.daqaggregator.data.mixin.ref.BUSummaryMixIn.class);
-		objectMapper.addMixIn(DAQ.class, rcms.utilities.daqaggregator.data.mixin.ref.DAQMixIn.class);
-		objectMapper.addMixIn(FED.class, rcms.utilities.daqaggregator.data.mixin.ref.FEDMixIn.class);
-		objectMapper.addMixIn(FEDBuilder.class, rcms.utilities.daqaggregator.data.mixin.ref.FEDBuilderMixIn.class);
-		objectMapper.addMixIn(FEDBuilderSummary.class, rcms.utilities.daqaggregator.data.mixin.ref.FEDBuilderSummaryMixIn.class);
-		objectMapper.addMixIn(FMM.class, rcms.utilities.daqaggregator.data.mixin.ref.FMMMixIn.class);
-		objectMapper.addMixIn(FMMApplication.class, rcms.utilities.daqaggregator.data.mixin.ref.FMMApplicationMixIn.class);
-		objectMapper.addMixIn(FRL.class, rcms.utilities.daqaggregator.data.mixin.ref.FRLMixIn.class);
-		objectMapper.addMixIn(FRLPc.class, rcms.utilities.daqaggregator.data.mixin.ref.FRLPcMixIn.class);
-		objectMapper.addMixIn(RU.class, rcms.utilities.daqaggregator.data.mixin.ref.RUMixIn.class);
-		objectMapper.addMixIn(SubFEDBuilder.class, rcms.utilities.daqaggregator.data.mixin.ref.SubFEDBuilderMixIn.class);
-		objectMapper.addMixIn(SubSystem.class, rcms.utilities.daqaggregator.data.mixin.ref.SubSystemMixIn.class);
-		objectMapper.addMixIn(TTCPartition.class, rcms.utilities.daqaggregator.data.mixin.ref.TTCPartitionMixIn.class);
-	}
-	
-	private void addMixins(ObjectMapper objectMapper){
-		objectMapper.addMixIn(BU.class, BUMixIn.class);
-		objectMapper.addMixIn(BUSummary.class, BUSummaryMixIn.class);
-		objectMapper.addMixIn(DAQ.class, DAQMixIn.class);
-		objectMapper.addMixIn(FED.class, FEDMixIn.class);
-		objectMapper.addMixIn(FEDBuilder.class, FEDBuilderMixIn.class);
-		objectMapper.addMixIn(FEDBuilderSummary.class, FEDBuilderSummaryMixIn.class);
-		objectMapper.addMixIn(FMM.class, FMMMixIn.class);
-		objectMapper.addMixIn(FMMApplication.class, FMMApplicationMixIn.class);
-		objectMapper.addMixIn(FRL.class, FRLMixIn.class);
-		objectMapper.addMixIn(FRLPc.class, FRLPcMixIn.class);
-		objectMapper.addMixIn(RU.class, RUMixIn.class);
-		objectMapper.addMixIn(SubFEDBuilder.class, SubFEDBuilderMixIn.class);
-		objectMapper.addMixIn(SubSystem.class, SubSystemMixIn.class);
-		objectMapper.addMixIn(TTCPartition.class, TTCPartitionMixIn.class);
-	}
-
-
-	public String serializeToSmile(DAQ daq, String name, String folder)
+	/**
+	 * Serialize DAQ snapshot object with given format
+	 * 
+	 * @param daqSnapshot
+	 *            DAQ snapshot object to be serialized
+	 * @param baseDir
+	 *            base directory where daqSnapshot will be serialized
+	 * @param format
+	 *            format in which daqSnapshot will be serialized
+	 * @return absolute path to serialized file
+	 * @throws IOException
+	 * @throws JsonMappingException
+	 * @throws JsonGenerationException
+	 */
+	public String serialize(DAQ daqSnapshot, String baseDir, SnapshotFormat format)
 			throws JsonGenerationException, JsonMappingException, IOException {
-		File file = new File(folder + name + ".smile");
-		ObjectMapper objectMapper = new ObjectMapper(new SmileFactory());
 
-		addMixins(objectMapper);
+		Date current = new Date(daqSnapshot.getLastUpdate());
+		createTimeDirs(baseDir, current);
+		ObjectMapper mapper = null;
+		String extension = null;
 
-		objectMapper.writeValue(file, daq);
-		return file.getAbsolutePath();
-	}
-
-	public String serializeToJSON(DAQ daqSnapshot, String name, String folder)
-			throws JsonGenerationException, JsonMappingException, IOException {
-		File file = new File(folder + name + ".json");
-		ObjectMapper mapper = new ObjectMapper();
-
+		switch (format) {
+		case SMILE:
+			mapper = new ObjectMapper(new SmileFactory());
+			extension = ".smile";
+			break;
+		case JSON:
+			mapper = new ObjectMapper();
+			extension = ".json";
+			break;
+		default:
+			logger.warn("Format of snapshot not available");
+		}
 		addMixins(mapper);
+
+		String snapshotFilename = current.getTime() + extension;
+		File file = new File(getTimeDir(baseDir, current) + snapshotFilename);
+
 		mapper.writerWithDefaultPrettyPrinter().writeValue(file, daqSnapshot);
 
 		return file.getAbsolutePath();
 	}
 
-	public String serializeToRefJSON(DAQ daqSnapshot, String name, String folder)
-			throws JsonGenerationException, JsonMappingException, IOException {
-		File file = new File(folder + name + ".json");
-		ObjectMapper mapper = new ObjectMapper();
-
-		addRefMixins(mapper);
-		mapper.writerWithDefaultPrettyPrinter().writeValue(file, daqSnapshot);
-
-		return file.getAbsolutePath();
-	}
-	
 	public DAQ deserializeFromSmile(String filepath) {
 		return mapperDeserialiser(filepath, new SmileFactory());
 	}
 
 	public DAQ deserializeFromJSON(String filepath) {
 		return mapperDeserialiser(filepath, new JsonFactory());
+	}
+
+	/**
+	 * Create time-base directory structure for given date
+	 * 
+	 * @param baseDir
+	 *            base directory where time-based directory will be situated
+	 * @param date
+	 *            timestamp for which time directory will be created
+	 */
+	private void createTimeDirs(String baseDir, Date date) {
+
+		File files = new File(getTimeDir(baseDir, date));
+		if (!files.exists()) {
+			if (files.mkdirs()) {
+				logger.info("Time-based directories created successfully");
+			} else {
+				throw new RuntimeException("Failed to create following dir: " + files.getAbsolutePath());
+			}
+		}
+	}
+
+	/**
+	 * Get the directory based on given date
+	 * 
+	 * @param baseDir
+	 *            base directory where time-based directory will be situated
+	 * @param date
+	 *            timestamp for which time directory will be returned
+	 * @return absolute path to time-based directory
+	 */
+	public String getTimeDir(String baseDir, Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH) + 1;
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+
+		logger.trace("Date: " + date);
+		logger.trace(year + ", " + month + ", " + day + ", " + hour);
+
+		String result = baseDir + year + "/" + month + "/" + day + "/" + hour + "/";
+		return result;
 	}
 
 	private DAQ mapperDeserialiser(String filepath, JsonFactory factory) {
@@ -137,7 +158,7 @@ public class StructureSerializer {
 		addMixins(mapper);
 
 		addMixins(mapper);
-		
+
 		ObjectInputStream in = null;
 		FileInputStream fileIn = null;
 		try {
@@ -159,5 +180,64 @@ public class StructureSerializer {
 				} catch (IOException e1) {
 				}
 		}
+	}
+
+	/**
+	 * Add mixin objects to object mapper
+	 * 
+	 * @param objectMapper
+	 */
+	private void addRefMixins(ObjectMapper objectMapper) {
+		objectMapper.addMixIn(BU.class, rcms.utilities.daqaggregator.data.mixin.ref.BUMixIn.class);
+		objectMapper.addMixIn(BUSummary.class, rcms.utilities.daqaggregator.data.mixin.ref.BUSummaryMixIn.class);
+		objectMapper.addMixIn(DAQ.class, rcms.utilities.daqaggregator.data.mixin.ref.DAQMixIn.class);
+		objectMapper.addMixIn(FED.class, rcms.utilities.daqaggregator.data.mixin.ref.FEDMixIn.class);
+		objectMapper.addMixIn(FEDBuilder.class, rcms.utilities.daqaggregator.data.mixin.ref.FEDBuilderMixIn.class);
+		objectMapper.addMixIn(FEDBuilderSummary.class,
+				rcms.utilities.daqaggregator.data.mixin.ref.FEDBuilderSummaryMixIn.class);
+		objectMapper.addMixIn(FMM.class, rcms.utilities.daqaggregator.data.mixin.ref.FMMMixIn.class);
+		objectMapper.addMixIn(FMMApplication.class,
+				rcms.utilities.daqaggregator.data.mixin.ref.FMMApplicationMixIn.class);
+		objectMapper.addMixIn(FRL.class, rcms.utilities.daqaggregator.data.mixin.ref.FRLMixIn.class);
+		objectMapper.addMixIn(FRLPc.class, rcms.utilities.daqaggregator.data.mixin.ref.FRLPcMixIn.class);
+		objectMapper.addMixIn(RU.class, rcms.utilities.daqaggregator.data.mixin.ref.RUMixIn.class);
+		objectMapper.addMixIn(SubFEDBuilder.class,
+				rcms.utilities.daqaggregator.data.mixin.ref.SubFEDBuilderMixIn.class);
+		objectMapper.addMixIn(SubSystem.class, rcms.utilities.daqaggregator.data.mixin.ref.SubSystemMixIn.class);
+		objectMapper.addMixIn(TTCPartition.class, rcms.utilities.daqaggregator.data.mixin.ref.TTCPartitionMixIn.class);
+	}
+
+	/**
+	 * Add mixin objects to object mapper
+	 * 
+	 * @param objectMapper
+	 */
+	private void addMixins(ObjectMapper objectMapper) {
+		objectMapper.addMixIn(BU.class, BUMixIn.class);
+		objectMapper.addMixIn(BUSummary.class, BUSummaryMixIn.class);
+		objectMapper.addMixIn(DAQ.class, DAQMixIn.class);
+		objectMapper.addMixIn(FED.class, FEDMixIn.class);
+		objectMapper.addMixIn(FEDBuilder.class, FEDBuilderMixIn.class);
+		objectMapper.addMixIn(FEDBuilderSummary.class, FEDBuilderSummaryMixIn.class);
+		objectMapper.addMixIn(FMM.class, FMMMixIn.class);
+		objectMapper.addMixIn(FMMApplication.class, FMMApplicationMixIn.class);
+		objectMapper.addMixIn(FRL.class, FRLMixIn.class);
+		objectMapper.addMixIn(FRLPc.class, FRLPcMixIn.class);
+		objectMapper.addMixIn(RU.class, RUMixIn.class);
+		objectMapper.addMixIn(SubFEDBuilder.class, SubFEDBuilderMixIn.class);
+		objectMapper.addMixIn(SubSystem.class, SubSystemMixIn.class);
+		objectMapper.addMixIn(TTCPartition.class, TTCPartitionMixIn.class);
+	}
+
+	@Deprecated
+	public String serializeToRefJSON(DAQ daqSnapshot, String name, String folder)
+			throws JsonGenerationException, JsonMappingException, IOException {
+		File file = new File(folder + name + ".json");
+		ObjectMapper mapper = new ObjectMapper();
+
+		addRefMixins(mapper);
+		mapper.writerWithDefaultPrettyPrinter().writeValue(file, daqSnapshot);
+
+		return file.getAbsolutePath();
 	}
 }
