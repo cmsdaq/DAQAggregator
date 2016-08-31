a5f1ce7a3a6bc51ab37d3751fa650cb89883dfef
gladky <maciej.gladki@gmail.com>
2016-08-31 16:31:20 +0200

    feat(): new session retriever with tests

---
+++ src/main/java/rcms/utilities/daqaggregator/datasource/SessionRetriever.java
@@ -0,0 +1,106 @@
+package rcms.utilities.daqaggregator.datasource;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+
+import org.apache.commons.lang3.tuple.Triple;
+import org.apache.log4j.Logger;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+/**
+ * Retrieves session information from flashlist LEVEL_ZERO_STATIC
+ * 
+ * @author Maciej Gladki (maciej.szymon.gladki@cern.ch)
+ *
+ */
+public class SessionRetriever {
+
+	protected static final String FMURL_COLUMN_NAME = "FMURL";
+	protected static final String TIMESTAMP_COLUMN_NAME = "timestamp";
+	protected static final String SID_COLUMN_NAME = "SID";
+	protected static final String HWKEY_COLUMN_NAME = "HWCFG_KEY";
+
+	private final String filter1;
+	private final String filter2;
+
+	private static final SimpleDateFormat df2 = new SimpleDateFormat("EEE, MMM dd yyyy HH:mm:ss Z");
+	private static final Logger logger = Logger.getLogger(SessionRetriever.class);
+
+	protected static final String EXCEPTION_MISSING_ROW_MESSAGE = "Could not found appropriate row in flashist to determine session";
+	protected static final String EXCEPTION_OTHER_PROBLEM_MESSAGE = "Problem detecting the session";
+	protected static final String EXCEPTION_PARSING_DATE_PROBLEM_MESSAGE = "Could not parse timestamp from flashlist data";
+	protected static final String EXCEPTION_NO_DATA_MESSAGE = "Flashlist has no data";
+	protected static final String EXCEPTION_FLASHLIST_NULL_MESSAGE = "Flashlist cannot be null";
+	protected static final String EXCEPTION_WRONG_FLASHLIST_MESSAGE = "Wrong flashlist type supplied, expected level zero static";
+
+	public SessionRetriever(String filter1, String filter2) {
+		this.filter1 = filter1;
+		this.filter2 = filter2;
+	}
+
+	/**
+	 * Retrieve session information from flashlist
+	 * 
+	 * @param flashlist
+	 * @return triple containing session id, hardware configuration key,
+	 *         timestamp of last update
+	 */
+	public Triple<String, Integer, Long> retrieveSession(Flashlist flashlist) {
+
+		if (flashlist == null)
+			throw new RuntimeException(EXCEPTION_FLASHLIST_NULL_MESSAGE);
+		if (flashlist.getFlashlistType() != FlashlistType.LEVEL_ZERO_FM_STATIC)
+			throw new RuntimeException(EXCEPTION_WRONG_FLASHLIST_MESSAGE);
+		if (flashlist.getRowsNode() == null || flashlist.getRowsNode().size() == 0)
+			throw new RuntimeException(EXCEPTION_NO_DATA_MESSAGE);
+
+		Iterator<JsonNode> rowIterator = flashlist.getRowsNode().iterator();
+		Triple<String, Integer, Long> result = null;
+		boolean foundRowSatisfyingFilters = false;
+
+		while (rowIterator.hasNext()) {
+			JsonNode row = rowIterator.next();
+
+			String fmUrl = row.get(FMURL_COLUMN_NAME).asText();
+
+			/* Retrieve data only from rows containing filters */
+			if (fmUrl.contains(filter1) && fmUrl.contains(filter2)) {
+				foundRowSatisfyingFilters = true;
+
+				long timestamp = parseTimestamp(row.get(TIMESTAMP_COLUMN_NAME).asText());
+				int sid = row.get(SID_COLUMN_NAME).asInt();
+				String hwcfgKey = row.get(HWKEY_COLUMN_NAME).asText();
+
+				logger.info(timestamp + ", " + hwcfgKey + ", " + sid + ", " + fmUrl);
+
+				String dpsetPath = hwcfgKey.split(":")[0];
+				result = Triple.of(dpsetPath, sid, timestamp);
+			}
+
+		}
+
+		if (!foundRowSatisfyingFilters) {
+			throw new RuntimeException(EXCEPTION_MISSING_ROW_MESSAGE);
+		}
+		if (result != null) {
+			logger.info("Result of " + result);
+		} else {
+			throw new RuntimeException(EXCEPTION_OTHER_PROBLEM_MESSAGE);
+		}
+		return result;
+
+	}
+
+	private long parseTimestamp(String timestampString) {
+		try {
+			Date date = df2.parse(timestampString);
+			logger.info("Parsed date: " + date + ", from string: " + timestampString);
+			return date.getTime();
+		} catch (ParseException e) {
+			throw new RuntimeException(EXCEPTION_PARSING_DATE_PROBLEM_MESSAGE);
+		}
+	}
+}
