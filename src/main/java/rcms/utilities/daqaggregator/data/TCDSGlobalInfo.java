fe0562285bd9fa32d394537c8db93c603a074a55
Michail Vougioukas <michail.vougioukas@cern.ch>
2016-09-29 18:38:48 +0200

    feat(model): Data type for an object that holds all available, global information retrieved from the tcds flashlists

---
+++ src/main/java/rcms/utilities/daqaggregator/data/TCDSGlobalInfo.java
@@ -0,0 +1,12 @@
+package rcms.utilities.daqaggregator.data;
+
+
+public class TCDSGlobalInfo {
+
+	/**
+	 * Container of global information from the TCDS flashlists
+	 *
+	 * @author Michail Vougioukas (michail.vougioukas@cern.ch)
+	 */
+	
+}
