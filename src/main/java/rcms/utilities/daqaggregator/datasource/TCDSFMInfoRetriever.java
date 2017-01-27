ba96f03a4545bd4832c305b3326289ef5c6c2264
Michail Vougioukas <michail.vougioukas@cern.ch>
2017-01-27 12:00:26 +0100

    feat(): Definition of a new object to download and hold information from TCDSFM flashlists for PM controllers

---
+++ src/main/java/rcms/utilities/daqaggregator/datasource/TCDSFMInfoRetriever.java
@@ -0,0 +1,73 @@
+package rcms.utilities.daqaggregator.datasource;
+
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import rcms.utilities.daqaggregator.DAQException;
+import rcms.utilities.daqaggregator.DAQExceptionCode;
+
+/**
+ * Class to fetch (c|l)pm controller information (to be used at the beginning of aggregation)
+ *
+ * @author Michail Vougioukas (michail.vougioukas@cern.ch)
+ */
+
+public class TCDSFMInfoRetriever {
+
+	private final FlashlistRetriever flashlistRetriever;
+	
+	private String tcdsfm_fmUrl;
+	private String tcdsfm_pmContext;
+	private int tcdsfm_pmLid;
+	private String tcdsfm_pmService;
+
+	public TCDSFMInfoRetriever(FlashlistRetriever flashlistRetriever) {
+		this.flashlistRetriever = flashlistRetriever;
+	}
+	
+	/**
+	 * Gets TCDS info from flashlists
+	 */
+	protected void downloadInformation() {
+
+		Pair<Flashlist, Integer> tcdsFmRetrieveResult = flashlistRetriever
+				.retrieveFlashlist(FlashlistType.TCDSFM);
+
+		Flashlist tcdsFmFlashlist = tcdsFmRetrieveResult.getLeft();
+
+		setTcdsFmFlashlistValues(tcdsFmFlashlist);
+		  
+	}
+	
+	private void setTcdsFmFlashlistValues(Flashlist flashlist) {
+		if (flashlist == null)
+			throw new DAQException(DAQExceptionCode.FlashlistNull, "");
+		if (flashlist.getFlashlistType() != FlashlistType.TCDSFM)
+			throw new DAQException(DAQExceptionCode.WrongFlaslhist,
+					"Wrong flashlist type: " + flashlist.getFlashlistType()+" (TCDFM expected)");
+		if (flashlist.getRowsNode() == null || flashlist.getRowsNode().size() == 0)
+			throw new DAQException(DAQExceptionCode.EmptyFlashlistDetectingSession,
+					"Empty TCDSFM flashlist at timestamp " + flashlist.getRetrievalDate());
+		
+		this.tcdsfm_fmUrl = flashlist.getRowsNode().get(0).get("FMURL").asText();
+		this.tcdsfm_pmContext = flashlist.getRowsNode().get(0).get("pmContext").asText();
+		this.tcdsfm_pmLid = flashlist.getRowsNode().get(0).get("pmLID").asInt();
+		this.tcdsfm_pmService = flashlist.getRowsNode().get(0).get("pmService").asText();
+	}
+
+	public String getTcdsfm_fmUrl() {
+		return tcdsfm_fmUrl;
+	}
+
+	public String getTcdsfm_pmContext() {
+		return tcdsfm_pmContext;
+	}
+
+	public int getTcdsfm_pmLid() {
+		return tcdsfm_pmLid;
+	}
+
+	public String getTcdsfm_pmService() {
+		return tcdsfm_pmService;
+	}
+}
