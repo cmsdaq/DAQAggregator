38dfedbeb132c87bc423a4f789d37be8c3c3bb59
Michail Vougioukas <michail.vougioukas@cern.ch>
2017-04-03 18:33:13 +0200

    fix(): FED finder in FEROL40

--- src/main/java/rcms/utilities/daqaggregator/mappers/helper/FedInFrlGeoFinder.java
+++ src/main/java/rcms/utilities/daqaggregator/mappers/helper/FedInFrlGeoFinder.java
@@ -11,17 +11,17 @@ import rcms.utilities.daqaggregator.datasource.Flashlist;
 /**
  * FED geolocation finder in FRL tree
  * 
- * For more information see {@link ThreeElementGeoMatcher} documentation
  * 
- * @author Maciej Gladki (maciej.szymon.gladki@cern.ch)
- *
  */
 public class FedInFrlGeoFinder extends ThreeElementGeoMatcher<FED> {
-	
+
 	private final String ioKey;
-	
-	public FedInFrlGeoFinder(String ioKey){
+
+	private boolean isFerol40;
+
+	public FedInFrlGeoFinder(String ioKey, boolean isFerol40){
 		this.ioKey = ioKey;
+		this.isFerol40 = isFerol40;
 	}
 
 	@Override
@@ -66,25 +66,49 @@ public class FedInFrlGeoFinder extends ThreeElementGeoMatcher<FED> {
 	protected Map<String, Map<Integer, Map<Integer, JsonNode>>> prepareFlashlistMap(Flashlist flashlist) {
 		Map<String, Map<Integer, Map<Integer, JsonNode>>> flashlistMap = new HashMap<>();
 
-		for (JsonNode row : flashlist.getRowsNode()) {
+		if (!this.isFerol40){
+			for (JsonNode row : flashlist.getRowsNode()) {
 
-			String hostname = row.get(this.getFlashlistHostnameKey()).asText();
+				String hostname = row.get(this.getFlashlistHostnameKey()).asText();
 
-			hostname = ContextHelper.getHostnameFromContext(hostname);
-			Integer geoslot = row.get(this.getFlashlistGeoslotKey()).asInt();
+				hostname = ContextHelper.getHostnameFromContext(hostname);
+				Integer geoslot = row.get(this.getFlashlistGeoslotKey()).asInt();
 
-			if (!flashlistMap.containsKey(hostname)) {
-				flashlistMap.put(hostname, new HashMap<Integer, Map<Integer, JsonNode>>());
+				if (!flashlistMap.containsKey(hostname)) {
+					flashlistMap.put(hostname, new HashMap<Integer, Map<Integer, JsonNode>>());
+				}
+
+				if (!flashlistMap.get(hostname).containsKey(geoslot)) {
+					flashlistMap.get(hostname).put(geoslot, new HashMap<Integer, JsonNode>());
+				}
+
+				flashlistMap.get(hostname).get(geoslot).put(0, row);
+				flashlistMap.get(hostname).get(geoslot).put(1, row);
+
 			}
+		}else{
+			for (JsonNode row : flashlist.getRowsNode()) {
+				String hostname = row.get(this.getFlashlistHostnameKey()).asText();
 
-			if (!flashlistMap.get(hostname).containsKey(geoslot)) {
-				flashlistMap.get(hostname).put(geoslot, new HashMap<Integer, JsonNode>());
+				hostname = ContextHelper.getHostnameFromContext(hostname);
+				Integer geoslot = row.get(this.getFlashlistGeoslotKey()).asInt();
+
+				if (!flashlistMap.containsKey(hostname)) {
+					flashlistMap.put(hostname, new HashMap<Integer, Map<Integer, JsonNode>>());
+				}
+
+				if (!flashlistMap.get(hostname).containsKey(geoslot)) {
+					flashlistMap.get(hostname).put(geoslot, new HashMap<Integer, JsonNode>());
+				}
+
+				flashlistMap.get(hostname).get(geoslot).put(0, row);
+				flashlistMap.get(hostname).get(geoslot).put(1, row);
+				flashlistMap.get(hostname).get(geoslot).put(2, row);
+				flashlistMap.get(hostname).get(geoslot).put(3, row);
 			}
-
-			flashlistMap.get(hostname).get(geoslot).put(0, row);
-			flashlistMap.get(hostname).get(geoslot).put(1, row);
-
 		}
+
+
 		return flashlistMap;
 	}
 
