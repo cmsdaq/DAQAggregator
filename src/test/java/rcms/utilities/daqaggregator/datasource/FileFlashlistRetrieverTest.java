8907a284fc7d4416e6b4644cb884edbc743fae1a
gladky <maciej.gladki@gmail.com>
2017-06-21 13:32:19 +0200

    test(): update IT tests after flashlist deletion

--- src/test/java/rcms/utilities/daqaggregator/datasource/FileFlashlistRetrieverTest.java
+++ src/test/java/rcms/utilities/daqaggregator/datasource/FileFlashlistRetrieverTest.java
@@ -36,15 +36,15 @@ public class FileFlashlistRetrieverTest {
 	public void noMoreFlashlistsTest() throws IOException {
 		FileFlashlistRetriever retriever = new FileFlashlistRetriever(TEST_FLASHLISTS_DIR, PersistenceFormat.JSON);
 		retriever.prepare(DatatypeConverter.parseDateTime("2016-09-01T15:00:00Z").getTimeInMillis());
-		Assert.assertEquals(19, retriever.retrieveAllFlashlists(dummySessionId).size());
-		Assert.assertEquals(19, retriever.retrieveAllFlashlists(dummySessionId).size());
-		Assert.assertEquals(19, retriever.retrieveAllFlashlists(dummySessionId).size());
-		Assert.assertEquals(19, retriever.retrieveAllFlashlists(dummySessionId).size());
-		Assert.assertEquals(19, retriever.retrieveAllFlashlists(dummySessionId).size());
+		Assert.assertEquals(18, retriever.retrieveAllFlashlists(dummySessionId).size());
+		Assert.assertEquals(18, retriever.retrieveAllFlashlists(dummySessionId).size());
+		Assert.assertEquals(18, retriever.retrieveAllFlashlists(dummySessionId).size());
+		Assert.assertEquals(18, retriever.retrieveAllFlashlists(dummySessionId).size());
+		Assert.assertEquals(18, retriever.retrieveAllFlashlists(dummySessionId).size());
 
 		thrown.expect(RuntimeException.class);
 		thrown.expectMessage(is(FileFlashlistRetriever.EXCEPTION_NO_FLASHLISTS_AVAILABLE));
-		Assert.assertEquals(19, retriever.retrieveAllFlashlists(dummySessionId).size());
+		Assert.assertEquals(18, retriever.retrieveAllFlashlists(dummySessionId).size());
 	}
 
 	@Test
@@ -52,27 +52,27 @@ public class FileFlashlistRetrieverTest {
 		FileFlashlistRetriever retriever = new FileFlashlistRetriever(TEST_FLASHLISTS_DIR, PersistenceFormat.JSON);
 		retriever.prepare(DatatypeConverter.parseDateTime("2016-09-01T15:00:00Z").getTimeInMillis());
 		Map<FlashlistType, Flashlist> result = retriever.retrieveAllFlashlists(dummySessionId);
-		Assert.assertEquals(19, result.size());
+		Assert.assertEquals(18, result.size());
 		Date d1 = result.get(FlashlistType.BU).getRetrievalDate();
 		Assert.assertEquals(1472743072594L, d1.getTime());
 
 		result = retriever.retrieveAllFlashlists(dummySessionId);
-		Assert.assertEquals(19, result.size());
+		Assert.assertEquals(18, result.size());
 		Date d2 = result.get(FlashlistType.BU).getRetrievalDate();
 		Assert.assertEquals(1472743080834L, d2.getTime());
 
 		result = retriever.retrieveAllFlashlists(dummySessionId);
-		Assert.assertEquals(19, result.size());
+		Assert.assertEquals(18, result.size());
 		Date d3 = result.get(FlashlistType.BU).getRetrievalDate();
 		Assert.assertEquals(1472743088565L, d3.getTime());
 
 		result = retriever.retrieveAllFlashlists(dummySessionId);
-		Assert.assertEquals(19, result.size());
+		Assert.assertEquals(18, result.size());
 		Date d4 = result.get(FlashlistType.BU).getRetrievalDate();
 		Assert.assertEquals(1472743097506L, d4.getTime());
 
 		result = retriever.retrieveAllFlashlists(dummySessionId);
-		Assert.assertEquals(19, result.size());
+		Assert.assertEquals(18, result.size());
 		Date d5 = result.get(FlashlistType.BU).getRetrievalDate();
 		Assert.assertEquals(1472743106576L, d5.getTime());
 
@@ -84,19 +84,19 @@ public class FileFlashlistRetrieverTest {
 		retriever.prepare(DatatypeConverter.parseDateTime("2016-09-01T15:00:00Z").getTimeInMillis());
 		Assert.assertEquals(FlashlistType.LEVEL_ZERO_FM_STATIC,
 				retriever.retrieveFlashlist(FlashlistType.LEVEL_ZERO_FM_STATIC).getLeft().getFlashlistType());
-		Assert.assertEquals(19, retriever.retrieveAllFlashlists(dummySessionId).size());
+		Assert.assertEquals(18, retriever.retrieveAllFlashlists(dummySessionId).size());
 		Assert.assertEquals(FlashlistType.LEVEL_ZERO_FM_STATIC,
 				retriever.retrieveFlashlist(FlashlistType.LEVEL_ZERO_FM_STATIC).getLeft().getFlashlistType());
-		Assert.assertEquals(19, retriever.retrieveAllFlashlists(dummySessionId).size());
+		Assert.assertEquals(18, retriever.retrieveAllFlashlists(dummySessionId).size());
 		Assert.assertEquals(FlashlistType.LEVEL_ZERO_FM_STATIC,
 				retriever.retrieveFlashlist(FlashlistType.LEVEL_ZERO_FM_STATIC).getLeft().getFlashlistType());
-		Assert.assertEquals(19, retriever.retrieveAllFlashlists(dummySessionId).size());
+		Assert.assertEquals(18, retriever.retrieveAllFlashlists(dummySessionId).size());
 		Assert.assertEquals(FlashlistType.LEVEL_ZERO_FM_STATIC,
 				retriever.retrieveFlashlist(FlashlistType.LEVEL_ZERO_FM_STATIC).getLeft().getFlashlistType());
-		Assert.assertEquals(19, retriever.retrieveAllFlashlists(dummySessionId).size());
+		Assert.assertEquals(18, retriever.retrieveAllFlashlists(dummySessionId).size());
 		Assert.assertEquals(FlashlistType.LEVEL_ZERO_FM_STATIC,
 				retriever.retrieveFlashlist(FlashlistType.LEVEL_ZERO_FM_STATIC).getLeft().getFlashlistType());
-		Assert.assertEquals(19, retriever.retrieveAllFlashlists(dummySessionId).size());
+		Assert.assertEquals(18, retriever.retrieveAllFlashlists(dummySessionId).size());
 
 	}
 }
