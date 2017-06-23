34adfccd4f51e31cf671a8516e1f3d65adf86cfa
Andre Holzner <holzner@lpcmssd02.dyndns.cern.ch>
2017-06-23 11:35:03 +0200

    introduced a second method HardwareConnector.initialize() taking a Properties as argument

--- src/main/java/rcms/utilities/daqaggregator/datasource/HardwareConnector.java
+++ src/main/java/rcms/utilities/daqaggregator/datasource/HardwareConnector.java
@@ -1,10 +1,11 @@
 package rcms.utilities.daqaggregator.datasource;
 
+import java.util.Properties;
 import rcms.common.db.DBConnectorException;
 import rcms.common.db.DBConnectorIF;
 import rcms.common.db.DBConnectorMySQL;
 import rcms.common.db.DBConnectorOracle;
-import rcms.utilities.daqaggregator.Application;
+import rcms.utilities.daqaggregator.Settings;
 import rcms.utilities.hwcfg.HWCfgConnector;
 import rcms.utilities.hwcfg.HWCfgDescriptor;
 import rcms.utilities.hwcfg.HardwareConfigurationException;
@@ -38,5 +39,18 @@ public class HardwareConnector {
 			_dbconn = new DBConnectorMySQL(url, user, passwd);
 
 		_hwconn = new HWCfgConnector(_dbconn);
+	}
+
+	public void initialize(Properties prop) throws DBConnectorException,
+					HardwareConfigurationException {
+
+		String url     = prop.getProperty(Settings.HWCFGDB_DBURL.getKey());
+		String host    = prop.getProperty(Settings.HWCFGDB_HOST.getKey());
+		String port    = prop.getProperty(Settings.HWCFGDB_PORT.getKey());
+		String sid     = prop.getProperty(Settings.HWCFGDB_SID.getKey());
+		String user    = prop.getProperty(Settings.HWCFGDB_LOGIN.getKey());
+		String passwd  = prop.getProperty(Settings.HWCFGDB_PWD.getKey());
+
+		initialize(url, host, port, sid, user, passwd);
 	}
 }
