e44b3042098eeafda2614e1277a81e39db356c7b
mgladki <maciej.gladki@gmail.com>
2017-11-06 15:48:24 +0100

    feat(): add HltInfo structure

---
+++ src/main/java/rcms/utilities/daqaggregator/data/HltInfo.java
@@ -0,0 +1,14 @@
+package rcms.utilities.daqaggregator.data;
+
+public class HltInfo  {
+
+    private int crashes;
+
+    public int getCrashes() {
+        return crashes;
+    }
+
+    public void setCrashes(int crashes) {
+        this.crashes = crashes;
+    }
+}
