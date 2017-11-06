5712df0c90474ce4f4a88c9f2f67542987c2624b
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2017-11-06 16:56:26 +0100

    added a new constant for the F3 cpuload script url to class Settings

--- src/main/java/rcms/utilities/daqaggregator/Settings.java
+++ src/main/java/rcms/utilities/daqaggregator/Settings.java
@@ -22,6 +22,7 @@ public enum Settings {
 	F3_HLT_URL("f3.htl.url"),
 	F3_DISK_URL("f3.disk.url"),
 	F3_CRASHES_URL("f3.crashes.url"),
+	F3_CPU_LOAD_URL("f3.cpuload.url"),
 
 	// settings for monitoring
 	MONITOR_SETUPNAME("monitor.setupName"),
