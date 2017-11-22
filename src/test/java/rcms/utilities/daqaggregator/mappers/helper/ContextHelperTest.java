f29630246e98dbc9aa2bc1bbeff006f059d1a619
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2017-11-22 16:32:26 +0100

    replaced .cms by .tld in ContextHelperTest, added initializer for application wide network domain

--- src/test/java/rcms/utilities/daqaggregator/mappers/helper/ContextHelperTest.java
+++ src/test/java/rcms/utilities/daqaggregator/mappers/helper/ContextHelperTest.java
@@ -1,31 +1,37 @@
 package rcms.utilities.daqaggregator.mappers.helper;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ContextHelperTest {
 
+	@BeforeClass
+	public static void setup() {
+		ContextHelper.setNetworkSuffix(".tld");
+	}
+	
 	@Test
 	public void hostnameSimpleTest() {
-		String actual = ContextHelper.getHostnameFromContext("abc.cms:123");
-		Assert.assertEquals("abc.cms", actual);
+		String actual = ContextHelper.getHostnameFromContext("abc.tld:123");
+		Assert.assertEquals("abc.tld", actual);
 	}
 
 	@Test
 	public void hostnameProtocolHttpTest() {
-		String actual = ContextHelper.getHostnameFromContext("http://abc.cms:123");
-		Assert.assertEquals("abc.cms", actual);
+		String actual = ContextHelper.getHostnameFromContext("http://abc.tld:123");
+		Assert.assertEquals("abc.tld", actual);
 	}
 
 	@Test
 	public void hostnameProtocolHttpsTest() {
-		String actual = ContextHelper.getHostnameFromContext("https://abc.cms:123");
-		Assert.assertEquals("abc.cms", actual);
+		String actual = ContextHelper.getHostnameFromContext("https://abc.tld:123");
+		Assert.assertEquals("abc.tld", actual);
 	}
 
 	@Test
 	public void portTest() {
-		int actual = ContextHelper.getPortFromContext("https://abc.cms:123");
+		int actual = ContextHelper.getPortFromContext("https://abc.tld:123");
 		Assert.assertEquals(123, actual);
 	}
 }
